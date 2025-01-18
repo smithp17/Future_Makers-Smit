90c2d7a6d1c48b8011d5d99d429b4dbe90dc037e
Smit Patne <smithp17999@gmail.com>
2025-01-18 11:47:11 -0500

    Some Images

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   const getImagePath = (index) => {
     const images = [
       '/images/image1.jpg',
-      '/images/image2.jpeg',
+      '/images/image2.jpg',
       '/images/image3.jpg',
       '/images/image4.jpg',
       '/images/image5.jpg',
